c606df1e56e2f9d51d66c8578480de4ddb82a23d
Alex Wills <77563588+AlexWills37@users.noreply.github.com>
2024-03-30 16:23:42 -0400

    texture based grid data

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
+import * as twgl from 'twgl.js/dist/5.x/twgl-full.js'
 
 function resizeCanvasToDisplaySize(canvas) {
     // Lookup the size the browser is displaying the canvas in CSS pixels.
@@ -46,64 +47,90 @@ function createProgram(gl, vertexShader, fragmentShader) {
     gl.deleteProgram(program);
 }
 
+
 function main() {
+    
     var canvas = document.querySelector("#canvas");
-    var gl = canvas.getContext("webgl");
+
+    /**@type {WebGLRenderingContext} */
+    const gl = canvas.getContext("webgl");
     if(!gl) {
         const warning = WebGL.getWebGLErrorMessage();
         document.body.appendChild(warning);
     }
 
+    
     // Specify the viewport size to convert from clip space [-1, 1] to canvas pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     // Create shader
-    var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
-    var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-    var program = createProgram(gl, vertexShader, fragmentShader);
+    var vsSource = document.querySelector("#vertex-shader-2d").text;
+    var fsSource = document.querySelector("#fragment-shader-2d").text;
+    const programInfo = twgl.createProgramInfo(gl,[vsSource, fsSource]);
 
-    // Find locations in shader
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
-    var positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // Create buffers
 
-    var positions = [
-        0, 0,
-        0, 0.5,
-        0.7, 0,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    const arrays ={
+        a_Position: {numComponents: 2, data:[
+            -0.5, -1,
+            0.5, -1,
+            0.5, 1,
+            -0.5, 1]},
+        a_GridID: {numComponents: 2, data: [0, 0, 10, 0, 10, 20, 0, 20]},
+        indices: {numComponents: 3, data: [0, 1, 2, 2, 3, 0]}
+    };
+
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+    var gridDataTex = gl.createTexture();
+    var gridData = new Uint8Array(200 * 3);
+    gridData[0] = 160;
+    gridData[1] = 32;
+    gridData[2] = 240;
+    gridData[4] = 200;
+    gl.bindTexture(gl.TEXTURE_2D, gridDataTex);
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 10, 20, 0, gl.RGB, gl.UNSIGNED_BYTE, gridData);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
 
     // Render code
-    // Possibly resize canvas
-    resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    function render(time) {
+        // Resize canvas if it changes
+        twgl.resizeCanvasToDisplaySize(gl.canvas);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        gridData[0] = (Math.sin(time * 0.001) * 0.5 + 0.5) * 255;
+        gridData[1] = (Math.cos(time * 0.001) * 0.5 + 0.5) * 255;
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, 10, 20, gl.RGB, gl.UNSIGNED_BYTE, gridData);
+        // Create uniform list
+        const uniforms = {
+            u_Time: time * 0.001,
+            u_Resolution: [gl.canvas.width, gl.canvas.height],
+            u_GridData: gridDataTex,
+        };
+    
+        gl.useProgram(programInfo.program); // Bind the shader program
+        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);  // Enable/create the attribute pointers
+        twgl.setUniforms(programInfo, uniforms);    // Set the shader uniforms
+        twgl.drawBufferInfo(gl, bufferInfo);    // Call the appropriate draw function
+    
+        requestAnimationFrame(render);  // Request the next frame
+    }
+
+    requestAnimationFrame(render);  // Request the first frame
+    
 
     // Clear the canvas
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    //gl.clearColor(0, 0, 0, 0);
+    //gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Specify shader
-    gl.useProgram(program);
-
-    // Enable attributes
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    // Bind the position buffer.
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    
-    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-    var size = 2;          // 2 components per iteration
-    var type = gl.FLOAT;   // the data is 32bit floats
-    var normalize = false; // don't normalize the data
-    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-    var offset = 0;        // start at the beginning of the buffer
-    gl.vertexAttribPointer(
-        positionAttributeLocation, size, type, normalize, stride, offset)
-
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
+
+
 
 main();
